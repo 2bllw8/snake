e6e9aa54aa3ff4bb697742c79ed69392e35f9d6c
2bllw8 <2bllw8+git@gmail.com>
2021-01-23 23:33:12 +0100

    Improve cli gui
    Add "game ready" and "game over" screens with option to
    (re)start and quit a game

--- cli/gui.c
+++ cli/gui.c
@@ -4,22 +4,38 @@
 #include <pthread.h>
 #include <stdatomic.h>
 #include <stdbool.h>
+#include <stdint.h>
 
 #define FRAME_INTERVAL 200
 
+typedef enum {
+	STATUS_READY,
+	STATUS_PLAYING,
+	STATUS_OVER,
+	STATUS_END,
+} status_t;
+
+typedef struct {
+	status_t *status;
+	game_t *game;
+} key_listener_args;
+
 atomic_bool listen_keys;
 
-void draw_game(game_t *game)
+void print_line_divider(uint8_t width)
 {
-	clear();
-	printw("Score: %u\n", game_score(game));
-
 	printw("+");
-	for (uint8_t x = 0; x < game->width; x++) {
+	for (uint8_t x = 0; x < width; x++) {
 		printw("-");
 	}
 	printw("+\n");
+}
 
+status_t game_play(game_t *game)
+{
+	move(0, 0);
+	printw("Your score: %u\n", game_score(game));
+	print_line_divider(game->width);
 	for (uint8_t y = 0; y < game->height; y++) {
 		printw("|");
 		for (uint8_t x = 0; x < game->width; x++) {
@@ -40,30 +56,57 @@ void draw_game(game_t *game)
 		}
 		printw("|\n");
 	}
+	print_line_divider(game->width);
 
-	printw("+");
-	for (uint8_t x = 0; x < game->width; x++) {
-		printw("-");
+	return game_frame(game) ? STATUS_PLAYING : STATUS_OVER;
+}
+
+status_t game_start()
+{
+	clear();
+	printw("The snake game\n");
+	printw("Press\n - 's' to start\n - 'q' to quit\n\n ? ");
+	char c = getch();
+	switch (c) {
+	case 's':
+		return STATUS_PLAYING;
+	case 'q':
+		return STATUS_END;
+	default:
+		return STATUS_READY;
 	}
-	printw("+\n");
 }
 
-bool frame(game_t *game)
+status_t game_over(game_t *game)
 {
-	draw_game(game);
-	return game_frame(game);
+	clear();
+	printw("Game over!\n");
+	printw("Your score: %d\n\n", game_score(game));
+	printw("Press\n - 's' to play again\n - 'q' to quit\n ? ");
+	switch (getch()) {
+	case 's':
+		game_reset(game);
+		return STATUS_PLAYING;
+	case 'q':
+		return STATUS_END;
+	default:
+		return STATUS_OVER;
+	}
 }
 
-void *keys_listener(void *args)
+void *keys_listener(void *extra)
 {
-	game_t *game = (game_t *)args;
+	key_listener_args *args = (key_listener_args *) extra;
 	while (listen_keys) {
+		if (*args->status != STATUS_PLAYING) {
+			continue;
+		}
 		switch (getch()) {
 		case KEY_LEFT:
-			game_turn_left(game);
+			game_turn_left(args->game);
 			break;
 		case KEY_RIGHT:
-			game_turn_right(game);
+			game_turn_right(args->game);
 			break;
 		}
 	}
@@ -87,16 +130,26 @@ void screen_teardown()
 void run(game_t *game)
 {
 	screen_setup();
+	status_t status = STATUS_READY;
 
+	key_listener_args args = {.status = &status, .game = game};
 	// Listen for keys in another thread
 	pthread_t keys_thread;
 	listen_keys = true;
-	pthread_create(&keys_thread, 0, keys_listener, (void *)game);
+	pthread_create(&keys_thread, 0, keys_listener, (void *)&args);
 
-	while (frame(game)) {
+	while (status != STATUS_END) {
+		if (status == STATUS_READY) {
+			status = game_start();
+		} else if (status == STATUS_PLAYING) {
+			status = game_play(game);
+		} else if (status == STATUS_OVER) {
+			status = game_over(game);
+		}
 		napms(FRAME_INTERVAL);
 	}
-
+	
 	listen_keys = false;
+	pthread_join(keys_thread, 0);
 	screen_teardown();
 }
